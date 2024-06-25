e357cec1c00eaf0cb10d6e5d50e8ffbad9a0955a
ffolabear <ffolabear@gmail.com>
2024-06-26 01:56:12 +0900

    feat : 스트림 퀴즈1 풀이 추가

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -3,28 +3,53 @@ package com.mangkyu.stream.Quiz1;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> collect = csvLines.stream()
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobby -> Arrays.stream(hobby.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, Integer::sum));
+        return collect;
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        Map<String, Integer> collect = csvLines.stream()
+                .filter(line -> line[0].startsWith("정"))
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobby -> Arrays.stream(hobby.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, Integer::sum));
+        System.out.println(collect);
+        return collect;
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return 0;
+        long result = csvLines.stream()
+                .map(line -> {
+                    int count = 0;
+                    int fromIndex = 0;
+                    while (fromIndex >= 0) {
+                        fromIndex = line[2].indexOf("좋아", fromIndex);
+                        if (fromIndex >= 0) {
+                            count++;
+                            fromIndex += 2;
+                        }
+                    }
+                    return count;
+                }).reduce(0, Integer::sum);
+        return (int) result;
     }
 
     private List<String[]> readCsvLines() throws IOException {
@@ -33,4 +58,10 @@ public class Quiz1 {
         return csvReader.readAll();
     }
 
+
+    public static void main(String[] args) throws IOException {
+        Quiz1 quiz1 = new Quiz1();
+//        quiz1.quiz1();
+        quiz1.quiz2();
+    }
 }
